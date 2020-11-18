fbfac4ec5aa9ad17f5ff587a81ac2b073c131178
yjyjyjy <prowessyang@gmail.com>
2020-11-17 17:41:22 -0800

    configured routes

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Mongo DB connected!!");
+  } catch (err) {
+    console.log(`MongoDB connection failed: ${err.message}`);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+module.exports = connectDB;
