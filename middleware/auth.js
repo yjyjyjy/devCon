b5e9f0f6c0f7aca099c5ad91f880e151d35699e1
yjyjyjy <prowessyang@gmail.com>
2020-11-19 23:30:08 -0800

    added auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = (req, res, next) => {
+  // Get the token from the header
+  const token = req.header("x-auth-token");
+
+  // check if no token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, auth denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
