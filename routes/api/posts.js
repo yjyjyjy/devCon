6b7e9e5c94164fd4febab123f360c054117ca071
yjyjyjy <prowessyang@gmail.com>
2020-11-25 14:44:05 -0800

    added get posts and delete post routes and functions

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,7 +6,7 @@ const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 const router = express.Router();
 
-// @route GET api/posts
+// @route POST api/posts
 // @desc Create a post
 // @access Private
 router.post(
@@ -30,9 +30,76 @@ router.post(
       res.json(post);
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("server error");
+      res.status(500).send("server error while creating a post");
     }
   }
 );
+
+// @route GET api/posts
+// @desc get all posts
+// @access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("server error while retrieving all posts");
+  }
+});
+
+// @route GET api/posts/:postId
+// @desc get post by id
+// @access Private
+router.get("/:postId", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ msg: "Retrieving post by id failed. Post not found" });
+    }
+    res.json(post);
+  } catch (error) {
+    if (error.kind === "ObjectId") {
+      // when input is not a valid object id.
+      return res.status(404).json({
+        msg: "Retrieving post by id failed. Input is not an Object Id",
+      });
+    }
+    console.error(error.message);
+    res.status(500).send("server error while retrieving post by id");
+  }
+});
+
+// @route DELETE api/posts/:postId
+// @desc delete post by id
+// @access Private
+router.delete("/:postId", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    if (!post) {
+      return res
+        .status(404)
+        .json({ msg: "Deleting post by id failed. Post not found" });
+    }
+    if (post.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: "Deleting post by id failed. Unauthroized user" });
+    }
+    await post.remove();
+    res.json({ msg: "post deleted" });
+  } catch (error) {
+    if (error.kind === "ObjectId") {
+      // when input is not a valid object id.
+      return res.status(404).json({
+        msg: "Deleting post by id failed. Input is not an Object Id",
+      });
+    }
+    console.error(error.message);
+    res.status(500).send("server error while deleting post by id");
+  }
+});
 
 module.exports = router;
