25c948861bcc7eb0e93bd4b3d32183e34fd798b4
yjyjyjy <prowessyang@gmail.com>
2020-12-02 10:54:48 -0800

    added front end router and auth routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./component/layout/Navbar";
 import Landing from "./component/layout/Landing";
 import "./App.css";
+import Register from "./component/auth/Register";
+import Login from "./component/auth/Login";
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
