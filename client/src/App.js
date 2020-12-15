0b9f1b919889f3fba37bde15b91d0f4d30779575
yjyjyjy <prowessyang@gmail.com>
2020-12-14 23:03:57 -0800

    set user token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./component/layout/Navbar";
 import Landing from "./component/layout/Landing";
@@ -6,28 +6,39 @@ import "./App.css";
 import Register from "./component/auth/Register";
 import Login from "./component/auth/Login";
 import Alert from "./component/layout/Alert";
+import { loadUser } from "./actions/auth";
 
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import setAuthToken from "./utils/setAuthToken";
 
+// check if there is a valid token in the local storage already. We can log user in if there is.
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,15 @@
+// Add the token to the header if there is one.
+
+import axios from "axios";
+
+// this method sync the local storage token with the default auth token sent with every request.
+const setAuthToken = (token) => {
+  if (token) {
+    // set default header wich will be sent with every reqeust being made. 
+    axios.defaults.headers.common["x-auth-token"] = token; 
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
+  }
+};
+
+export default setAuthToken;
