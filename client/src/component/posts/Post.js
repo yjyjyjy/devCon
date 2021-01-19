475369f3e070609c194ef6cde0ec530aac3e7fdb
yjyjyjy <prowessyang@gmail.com>
2021-01-19 09:04:36 -0800

    comments display

--- client/src/component/posts/Post.js
+++ client/src/component/posts/Post.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getPostById } from "../../actions/post";
 import { Link } from "react-router-dom";
+import PostCommentItem from "./PostCommentItem";
 
 const Post = ({
   match: {
@@ -15,24 +16,24 @@ const Post = ({
   return (
     !!post && (
       <Fragment>
-        <div class="post bg-white p-1 my-1">
+        <div className="post bg-white p-1 my-1">
           <div>
             <Link to={`/profile/${post.user}`}>
-              <img class="round-img" src={post.avatar} alt="" />
+              <img className="round-img" src={post.avatar} alt="" />
               <h4>{post.name}</h4>
             </Link>
           </div>
           <div>
-            <p class="my-1">{post.text}</p>
+            <p className="my-1">{post.text}</p>
           </div>
         </div>
 
         {/* comment Form */}
-        <div class="post-form">
-          <div class="bg-primary p">
+        <div className="post-form">
+          <div className="bg-primary p">
             <h3>Leave A Comment</h3>
           </div>
-          <form class="form my-1">
+          <form className="form my-1">
             <textarea
               name="text"
               cols="30"
@@ -40,35 +41,14 @@ const Post = ({
               placeholder="Comment on this post"
               required
             ></textarea>
-            <input type="submit" class="btn btn-dark my-1" value="Submit" />
+            <input type="submit" className="btn btn-dark my-1" value="Submit" />
           </form>
         </div>
 
         {/* comments */}
-        <div class="post bg-white p-1 my-1">
-          <div>
-            <a href="profile.html">
-              <img
-                class="round-img"
-                src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
-                alt=""
-              />
-              <h4>John Doe</h4>
-            </a>
-          </div>
-          <div>
-            <p class="my-1">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-              possimus corporis sunt necessitatibus! Minus nesciunt soluta
-              suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-              dolor? Illo perferendis eveniet cum cupiditate aliquam?
-            </p>
-            <p class="post-date">Posted on 04/16/2019</p>
-            <button type="button" class="btn btn-danger">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
-        </div>
+        {post.comments.map((comment) => (
+          <PostCommentItem key={comment._id} comment={comment} />
+        ))}
       </Fragment>
     )
   );
---
+++ client/src/component/posts/PostCommentItem.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+import { connect } from "react-redux";
+
+const PostCommentItem = ({
+  comment: { _id, avatar, date, name, text, user },
+  authUserId,
+}) => {
+  return (
+    <div className="post bg-white p-1 my-1">
+      <div>
+        <a href="profile.html">
+          <img className="round-img" src={avatar} alt="" />
+          <h4>{name}</h4>
+        </a>
+      </div>
+      <div>
+        <p className="my-1">{text}</p>
+        <p className="post-date">
+          Posted on <Moment format="MM/DD/YYYY">{date}</Moment>
+        </p>
+        {user === authUserId && (
+          <button type="button" className="btn btn-danger">
+            <i className="fas fa-times"></i>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+PostCommentItem.propTypes = { comment: PropTypes.object.isRequired };
+
+const mapStateToProps = (state) => ({
+  authUserId: state.auth.user._id,
+});
+
+export default connect(mapStateToProps)(PostCommentItem);
