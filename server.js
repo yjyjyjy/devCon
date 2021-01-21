ff33e6cb69eff684f0aaf48aa3736807139923d4
yjyjyjy <prowessyang@gmail.com>
2021-01-20 23:07:33 -0800

    deploy changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path"); // core node.js module to manipulate path
 
 const app = express();
 
@@ -9,13 +10,21 @@ connectDB();
 // a new way to do bodyParser
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API running"));
-
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// serve static asset in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build")); // set static folder to "client/build"
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  }); // for any other route except the routes specified above, /client/build/index.html will be served.
+}
 
 const PORT = process.env.PORT || 5000;
 
